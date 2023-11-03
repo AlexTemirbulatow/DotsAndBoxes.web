379200f6d69b49edbcd6f84b50d0256de222b561
Alextem <temirbulatow1@gmail.com>
2023-11-03 15:35:15 +0100

    futher dev

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -1,17 +1,16 @@
 package controllers
 
-import javax.inject._
-import play.api._
 import play.api.mvc._
+import javax.inject._
 
-import de.htwg.se.dotsandboxes.controller.controllerComponent.controllerImpl.Controller
-import de.htwg.se.dotsandboxes.model.fieldComponent.fieldImpl.Move
 import de.htwg.se.dotsandboxes.Default.given_FieldInterface
 import de.htwg.se.dotsandboxes.Default.given_FileIOInterface
+import de.htwg.se.dotsandboxes.model.fieldComponent.fieldImpl.Move
+import de.htwg.se.dotsandboxes.controller.controllerComponent.controllerImpl.Controller
 
 
 @Singleton
-class HomeController @Inject() (cc: ControllerComponents) extends AbstractController(cc):
+class HomeController @Inject()(cc: ControllerComponents) extends AbstractController(cc):
   val controller = new Controller()
   def getField = controller.toString
 
@@ -21,7 +20,8 @@ class HomeController @Inject() (cc: ControllerComponents) extends AbstractContro
 
   def move(input: String): Action[AnyContent] = Action {
     val chars = input.toCharArray
-    val move: Move = Move(chars(0).toString.toInt, chars(1).toString.toInt, chars(2).toString.toInt, true)
+    val move: Move = Move(chars(0).toString.toInt,
+    chars(1).toString.toInt, chars(2).toString.toInt, true)
     controller.publish(controller.put, move)
     Ok(views.html.field(controller))
   }
