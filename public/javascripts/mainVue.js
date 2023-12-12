fe65c2c2f6809983c517c180da7eb131d2508799
montaser717 <98126948+montaser717@users.noreply.github.com>
2023-12-12 11:43:08 +0100

    SPA works

--- public/javascripts/mainVue.js
+++ public/javascripts/mainVue.js
@@ -1,9 +1,12 @@
+import { root } from './rootVue.js'
+import { index } from './indexVue.js'
 import { gameBar } from './gameBarVue.js'
 import { gameboard } from './gameboardVue.js'
 import { gameFooter } from './gameFooterVue.js'
 
-const app = Vue.createApp({})
-app.component('game-board', gameboard)
-app.component('game-bar-nav', gameBar)
-app.component('game-footer', gameFooter)
-app.mount('#app')
+const app = Vue.createApp(root)
+app.component('index-page', index)
+.component('game-bar', gameBar)
+.component('game-board', gameboard)
+.component('game-footer', gameFooter)
+.mount('#app')
