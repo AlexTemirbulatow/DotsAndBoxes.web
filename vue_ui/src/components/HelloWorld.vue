0217851c3cec96d2455989d9036419f6feffa08c
Alextem <temirbulatow1@gmail.com>
2023-12-17 18:44:04 +0100

    init vue component

--- vue_ui/src/components/HelloWorld.vue
+++ vue_ui/src/components/HelloWorld.vue
@@ -1,61 +1,114 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-pwa" target="_blank" rel="noopener">pwa</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router" target="_blank" rel="noopener">router</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-vuex" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <h1>This is HelloWorld</h1>
+    
+    <div class="dot"></div>
+    
+    <h1>This is HelloWorld END</h1>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
+  name: 'HelloWorld'
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
+
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+
+.dot {
+    background-color: #ededed;
+    border: 2px solid #f51818;
+    box-shadow: 2px 7px 0px #5b5b5bb5;
+    width: 30px;
+    height: 30px;
+    border-radius: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    z-index: 3;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+
+.dab {
+    font-size: 8ex;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+
+.mini {
+    font-family: "Lilita One", sans-serif;
+    font-size: 3vh;
 }
-a {
-  color: #42b983;
+
+body h1, 
+body h2,
+body h5 {
+    font-family: "Agbalumo";
+    color: #d8d8d8;
 }
+
+.logo {
+    font-family: "Agbalumo";
+    font-size: 2.5ex;
+}
+
+#playbtn {
+    display: inline-block;
+    position: relative;
+    padding: 14px 40px;
+    margin-top: 20px;
+    color: #4a3e6dbc;
+    background-color: #0CFFE1;
+    box-shadow: 10px 10px 6px #4a3e6db6;
+    border-radius: 10px;
+    font-size: 30px;
+    font-weight: 500px;
+    letter-spacing: 3px;
+    text-transform: capitalize;
+    text-decoration: none;
+    overflow: hidden;
+    z-index: 1;
+}
+
+#playbtn span {
+    display: block;
+    width: 0px;
+    height: 0px;
+    background-color: #FF217C;
+    position: absolute;
+    border-radius: 50%;
+    transform: translate(-50%, -50%);
+    transition: width 0.6s ease 0s, height 0.6s ease 0s;
+    z-index: -1;
+}
+
+#playbtn:hover span {
+    width: 562px;
+    height: 562px;
+}
+
+.navbar-nav {
+    font-family: "Lilita One", sans-serif;
+}
+
+@media (max-width: 991px) {
+    body .sidebar {
+        background-color: rgba(255, 255, 255, 0.2);
+        backdrop-filter: blur(10px);
+    }
+}
+
+.navbar {
+    font-family: "Arial";
+    background-color: rgba(37, 37, 37, 0.545);
+}
+
+.operation {
+    color: #d8d8d8;
+    background: none;
+    border: none;
+    width: 50px;
+    height: 50px;
+    padding: 10px;
+}
+
 </style>
---
+++ vue_ui/src/views/GameView.vue
@@ -0,0 +1,48 @@
+<template>
+  <router-link to="/">Home</router-link> |
+  <router-link to="/game">Game</router-link>
+  <h1>This is a game page</h1>
+
+  <HelloWorld />
+
+  <div class="containerh pt-5 pb-3">
+    <template v-if="!gameEnded">
+      <div class="playerTurnImg" id="turn">
+        <img v-if="currentPlayer === 0" src='../assets/playerBlue.png'>
+        <img v-else-if="currentPlayer === 1" src='../assets/playerRed.png'>
+        <img v-else-if="currentPlayer === 2" src='../assets/playerGreen.png'>
+        <img v-else-if="currentPlayer === 3" src='../assets/playerYellow.png'>
+        <h1>Turn</h1>
+        <img src='../assets/playerBlue.png'>
+      </div>
+    </template>
+    <template v-else>
+      <div class="playerWonImg">
+        <template v-if="winner === 'It\'s a draw!'">
+          <h1>It's a draw!</h1>
+        </template>
+        <template v-else>
+          <img :src="'assets/images/player' + matchingWinner() + '.png'">
+          <h1>wins!</h1>
+        </template>
+      </div>
+    </template>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+.game {
+  background-color: burlywood;
+}
+</style>
