7202ae3871c8e63ded6d744f65efbd4082444089
Alextem <temirbulatow1@gmail.com>
2024-01-14 13:50:57 +0100

    added PWA

--- vue_ui/vue.config.js
+++ vue_ui/vue.config.js
@@ -9,5 +9,32 @@ module.exports = defineConfig({
       importStrategy: 'kebab',
       rtlSupport: false
     }
+  },
+
+  pwa: {
+    name: "DotsAndBoxes",
+    themeColor: "#483C6C",
+    mobileWebAppCapable: 'yes',
+    mobileWebAppCache: 'yes',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppCache: 'yes',
+    workboxPluginMode: 'GenerateSW',
+    workboxOptions: {
+      navigateFallback: '/index.html',
+      skipWaiting: true,
+      clientsClaim: true
+    },
+    manifestOptions: {
+      name: "DotsAndBoxes",
+      short_name: "DaB",
+      start_url: '.',
+      display: 'standalone',
+      theme_color: '#fefefe',
+      icons:[{
+        src:"./images/Logo_192x192.png",
+        sizes: "192x192",
+        type: "image/png"
+      }]
+    }
   }
 })
